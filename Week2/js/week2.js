eed44c8424652f80e5a133a0ade644652225931a
justinthomsen <justinrobertthomsen@gmail.com>
2019-02-21 21:35:43 -0600

    giant bad commit

---
+++ Week2/js/week2.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('#myCarousel').carousel({interval: 2000});
+    $('#carouselButton').click(function () {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#myCarousel').carousel('pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#myCarousel').carousel('cycle');
+            $('#carouselButton').children('span').addClass('fa-pause');
+            $('#carouselButton').children('span').removeClass('fa-play');
+        }
+    });
+});
